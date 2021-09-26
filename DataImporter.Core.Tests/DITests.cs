2d028f2079af42926feff92d0ee27b1691d7aecd
Emmanouil Gimissis <manolis@pgimisis.gr>
2021-09-26 15:11:42 +0100

    Added multiple tests

--- DataImporter.Core.Tests/DITests.cs
+++ DataImporter.Core.Tests/DITests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace DataImporter.Core.Tests
 {
-    public class UnitTest1
+    public class DITests
     {
         private string basePath = @"D:\temp\TestData\";
         private string connectionString = "Server=.;Database=IntReach;Trusted_Connection=True;MultipleActiveResultSets=true;";
@@ -19,11 +19,16 @@ namespace DataImporter.Core.Tests
             Assert.True(res == ImportResult.ImportOK);
         }
 
-        [Fact]
-        public void GetProductsTest()
+        [Theory]
+        [InlineData(100, 102)]
+        [InlineData(100, 105)]
+        [InlineData(200, 201)]
+        [InlineData(200, 102)]
+        [InlineData(300, 305)]
+        public void GetProductsTest(int companyID, int feedID)
         {
             ProductService svc = new ProductService(connectionString);
-            List<Product> products = svc.GetProducts(100, 102);
+            List<Product> products = svc.GetProducts(companyID, feedID);
             Assert.True(products.Count==150);
         }
     }
